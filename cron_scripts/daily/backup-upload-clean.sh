95a05b8cb4de8b054a73d22761f6f1fe6b29e326
Robbert Bos <robbert@askanna.io>
2025-03-10 06:36:25 +0100

    Fix daily cronjob config

--- cron_scripts/daily/backup-upload-clean.sh
+++ cron_scripts/daily/backup-upload-clean.sh
@@ -4,8 +4,12 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+echo -e "Starting daily backup procedure\n"
+
 gcs_upload
 backup_clean
 backup_postgres
 backup_files
 gcs_upload
+
+echo -e "\nFinished daily backup procedure"
